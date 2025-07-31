fec5b0d011f2e17072261bcd3227cdf066d4fd38
Edgar Castillo Vega <vayne.edgar@gmail.com>
2025-07-31 17:51:32 -0600

    [ECV-01] Feature: Some Improvements and Features [v1]

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/todos', function () {
+    return view('todos.index');
+});
+
+Route::get('/json', function () {
+    return ["id" => 22, "age" => 45];
+});
